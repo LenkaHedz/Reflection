ca82ad3dfc2558b5a4ec2e4870da87cda75b6565
Olena Hedz <Lenka-949@mail.ru>
2018-03-16 13:47:14 +0200

    Update Main.java

--- Computer/src/main/java/ua/training/Main.java
+++ Computer/src/main/java/ua/training/Main.java
@@ -7,18 +7,20 @@ public class Main {
     public static void main(String[] args) {
         Computer comp1 = new Computer("Lenovo", 360, Cores.ONE, 3.9,530);
         Notebook nout1 = new Notebook("Lenovo", 360, Cores.TWO, 2.5,280, 17.5, 2600, 480);
+        ReflectionMethods rm = new ReflectionMethods(comp1.getClass());
+        // Название пакета
+        rm.getPackageName();
+        // Которкое (простое) имя класса
+        rm.getSimpleClassName();
         // Список методов с аннотациями и типами параметров
-        // Название пакета и которкое (простое) имя класса
-        Class <?> cl = comp1.getClass();
-        System.out.println("All fields in class" + cl.getSimpleName() + "\n");
-        Method[] metods = cl.getMethods();
-        for (Method metod : metods) {
-            System.out.println(metod.getName() + " - " + metod.getDeclaredAnnotations().toString() + " - " + metod.getParameterTypes());
-        }
-        Field[] fields = cl.getDeclaredFields();
-        for (Field field : fields) {
-            Class <?> fieldClass = field.getType();
-            System.out.println(field.getName() + " - " + fieldClass.getName());
-        }
+        rm.getMetods();
+
+        rm = new ReflectionMethods(nout1.getClass());
+        // Название пакета
+        rm.getPackageName();
+        // Которкое (простое) имя класса
+        rm.getSimpleClassName();
+        // Список методов с аннотациями и типами параметров
+        rm.getMetods();
     }
 }
